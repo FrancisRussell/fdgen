001f284011e898b6ee17d4ab14148d35d76a8ff0
Francis Russell <francis@unchartedbackwaters.co.uk>
2016-07-03 21:49:41 +0100

    Implement basic declaration parsing.

--- compiler/FDGEN/Parser.hs
+++ compiler/FDGEN/Parser.hs
@@ -1,42 +1,135 @@
 module FDGEN.Parser (parseInput) where
-import Text.Parsec (ParseError, runParser, getState, Parsec)
+import Data.Map
+import Control.Applicative ((<$>))
+import Text.Parsec.Char (letter)
+import Text.Parsec.Combinator (eof, choice)
 import Text.Parsec.Language (emptyDef, LanguageDef)
+import Text.Parsec (ParseError, runParser, getState, Parsec, modifyState)
+import Text.Parsec.Prim (many, parserFail, (<|>))
 import Text.Parsec.Token (GenTokenParser(..), GenLanguageDef(..), makeTokenParser)
-import Data.Set (Set, empty)
+import qualified Data.Map as Map
 
 data FDFL = FDFL {
-  symbols :: Set String
+  symbols :: Map String Definition
 } deriving (Show)
 
 emptyFDFL :: FDFL
 emptyFDFL = FDFL {
-  symbols = empty
+  symbols = Map.empty
 }
 
 type FDFLParser a = Parsec String FDFL a
 
+data ObjectType
+ = FieldType
+
+data Field
+ = Field
+ deriving Show
+
+data Mesh
+ = Mesh
+ deriving Show
+
+data Definition
+ = FieldDef Field
+ | MeshDef Mesh
+ deriving Show
+
+data AttributeValue
+ = StringAttribute String
+ | IdentifierAttribute String
+ | IntegerAttribute Integer
+ | IdentifierListAttribute [String]
+ | BooleanAttribute Bool
+
+duplicateSymbolFail :: Show k => k -> a -> a -> a
+duplicateSymbolFail k _ _  = error $ "Attempted to redefine symbol " ++ (show k)
+
+addDefinition :: String -> Definition -> FDFL -> FDFL
+addDefinition symName def fdfl = fdfl { symbols = Map.insertWithKey duplicateSymbolFail symName def $ symbols fdfl}
+
 fdflDef :: LanguageDef st
 fdflDef = emptyDef {
   caseSensitive = True,
-  reservedOpNames = ["="]
+  reservedOpNames = ["="],
+  reservedNames = ["Field", "True", "False", "Mesh"],
+  commentLine = "#",
+  identStart = letter
 }
 
 TokenParser {
   identifier = parseIdentifier,
-  reservedOp = parseReservedOp
+  reservedOp = parseReservedOp,
+  reserved = parseReserved,
+  parens = parseParens,
+  commaSep = parseCommaSep,
+  stringLiteral = parseStringLiteral,
+  integer = parseInteger,
+  brackets = parseBrackets
 } = makeTokenParser fdflDef
 
 parseFDFL :: FDFLParser FDFL
 parseFDFL =
-  parseAssignment >>
+  many parseAssignment >>
+  eof >>
   getState
 
 parseAssignment :: FDFLParser ()
 parseAssignment = do
-  symbol <- parseIdentifier
+  symbolName <- parseIdentifier
   _ <- parseReservedOp "="
+  definition <- parseDefinition
+  modifyState $ addDefinition symbolName definition
   return ()
 
+parseDefinition :: FDFLParser Definition
+parseDefinition = FieldDef <$> parseFieldDefinition <|>
+                  MeshDef <$> parseMeshDefinition
+
+parseFieldDefinition :: FDFLParser Field
+parseFieldDefinition = do
+  parseReserved "Field"
+  attributes <- parseParens $ parseCommaSep parseAttribute
+  case attributesToField attributes of
+    Left str -> parserFail str
+    Right field -> return field
+
+parseMeshDefinition :: FDFLParser Mesh
+parseMeshDefinition = do
+  parseReserved "Mesh"
+  attributes <- parseParens $ parseCommaSep $ parseAttribute
+  case attributesToMesh attributes of
+    Left str -> parserFail str
+    Right mesh -> return mesh
+
+attributesToField :: [(String, AttributeValue)] -> Either String Field
+attributesToField _ = Right Field
+
+attributesToMesh :: [(String, AttributeValue)] -> Either String Mesh
+attributesToMesh _ = Right Mesh
+
+parseAttribute :: FDFLParser (String, AttributeValue)
+parseAttribute = do
+  name <- parseIdentifier
+  _ <- parseReservedOp "="
+  value <- parseAttributeValue
+  return (name, value)
+
+parseAttributeValue :: FDFLParser AttributeValue
+parseAttributeValue = choice
+  [ IdentifierAttribute <$> parseIdentifier
+  , StringAttribute <$> parseStringLiteral
+  , IntegerAttribute <$> parseInteger
+  , BooleanAttribute <$> parseBool
+  , IdentifierListAttribute <$> (parseBrackets $ parseCommaSep parseIdentifier)
+  ]
+
+parseBool :: FDFLParser Bool
+parseBool = choice
+  [ parseReserved "True" >> return True
+  , parseReserved "False" >> return False
+  ]
 
 parseInput :: String -> Either ParseError FDFL
 parseInput = runParser parseFDFL emptyFDFL ""
--- dsl
+++ dsl
@@ -1,13 +1,10 @@
-m = Mesh(name="shallow_water", dimension=2)
 V = Field(mesh=m, name="velocity", rank=1, staggered=False)
 H = Field(mesh=m, name="height", rank=0, staggered=True)
+m = Mesh(name="shallow_water", dimension=2, fields=[V, H])
 
-spatial_stagger=[None, All, Index]
-temporal_stagger=True
+#VelocityUpdate = FieldUpdate(mesh=m, V, V)
 
-VelocityUpdate = FieldUpdate(mesh=m, V, V)
-
-U = Update(mesh=m, name="step", spatial_order=1, temporal_order=1, updates=[VelocityUpdate])
+#U = Update(mesh=m, name="step", spatial_order=1, temporal_order=1, updates=[VelocityUpdate])
 
 
 
