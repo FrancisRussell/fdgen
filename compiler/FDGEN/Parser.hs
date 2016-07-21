35e5d2ef0116e5d585491c8148ebbf5511145b41
Francis Russell <francis@unchartedbackwaters.co.uk>
2016-07-21 17:56:03 +0100

    Rework parser implementation.

--- compiler/FDGEN/Parser.hs
+++ compiler/FDGEN/Parser.hs
@@ -1,35 +1,40 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, Rank2Types #-}
 module FDGEN.Parser (parseInput) where
-import Data.Map
-import Data.Set
+import Data.Map (Map)
 import Control.Applicative ((<$>))
-import Control.Monad (foldM)
 import Text.Parsec.Char (letter)
-import Text.Parsec.Combinator (eof, choice)
+import Text.Parsec.Combinator (eof, choice, optionMaybe)
 import Text.Parsec.Language (emptyDef, LanguageDef)
 import Text.Parsec (ParsecT, ParseError, runParser, getState, Parsec, putState)
-import Text.Parsec.Prim (many, parserFail, (<|>), parsecMap)
+import Text.Parsec.Prim (many, parserFail)
 import Text.Parsec.Token (GenTokenParser(..), GenLanguageDef(..), makeTokenParser)
 import qualified Control.Lens as Lens
 import qualified Data.Map as Map
-import qualified Data.Set as Set
+
+data Identifier = Identifier String
+  deriving Show
+
+data StringLiteral = StringLiteral String
+  deriving Show
 
 data Mesh = Mesh {
-  meshName :: String,
-  meshDimension :: Integer,
-  meshFields :: [String]
+  _meshName :: StringLiteral,
+  _meshDimension :: Integer,
+  _meshFields :: [Identifier]
 } deriving Show
 
 data Field = Field {
-  fieldName :: String,
-  fieldRank :: Integer,
-  fieldSymmetric :: Bool,
-  fieldTemporalStaggering :: [Bool],
-  fieldSpatialStaggering :: [[Bool]]
+  _fieldName :: StringLiteral,
+  _fieldRank :: Integer,
+  _fieldSymmetric :: Bool,
+  _fieldStaggerStrategySpatial :: StaggerStrategy,
+  _fieldStaggerStrategyTemporal :: StaggerStrategy
 } deriving Show
 
-data FieldUpdate = FieldUpdate String FieldExpr
-  deriving Show
+data FieldUpdate = FieldUpdate {
+  _fieldUpdateLHS :: Identifier,
+  _fieldUpdateRHS :: Identifier
+} deriving Show
 
 data FieldExpr = FieldRef String
   deriving Show
@@ -44,18 +49,20 @@ data StaggerStrategy
   = All
   | None
   | Dimension
-  deriving Show
+  deriving (Bounded, Enum, Show)
 
 data FDFL = FDFL {
   symbols :: Map String Definition
 } deriving Show
 
 data FDFLParseState = FDFLParseState {
-  _psFDFL :: FDFL,
-  psStaggering :: Map String StaggerStrategy
+  _psFDFL :: FDFL
 } deriving Show
 
 Lens.makeLenses ''FDFLParseState
+Lens.makeLenses ''Mesh
+Lens.makeLenses ''Field
+Lens.makeLenses ''FieldUpdate
 
 emptyFDFL :: FDFL
 emptyFDFL = FDFL {
@@ -64,79 +71,185 @@ emptyFDFL = FDFL {
 
 emptyFDFLParseState :: FDFLParseState
 emptyFDFLParseState = FDFLParseState {
-  _psFDFL = emptyFDFL,
-  psStaggering = Map.empty
+  _psFDFL = emptyFDFL
 }
 
 type FDFLParser a = Parsec String FDFLParseState a
 
-data MeshUpdate = MeshUpdate String [FieldUpdate]
-  deriving Show
+type Validator a = a -> FDFLParser a
 
-data AttributeValue
- = StringAttribute String
- | IdentifierAttribute String
- | IntegerAttribute Integer
- | IdentifierListAttribute [String]
- | BooleanAttribute Bool
+data AttributeUpdate s = AttributeUpdate String (s -> s)
 
-toStringAttribute :: AttributeValue -> Maybe String
-toStringAttribute (StringAttribute str) = Just str
-toStringAttribute _ = Nothing
+data PositionalSpec s = PositionalSpec String (FDFLParser (AttributeUpdate s))
 
-toIntegerAttribute :: AttributeValue -> Maybe Integer
-toIntegerAttribute (IntegerAttribute i) = Just i
-toIntegerAttribute _ = Nothing
+data AttributeSpec s = AttributeSpec String Bool (FDFLParser (AttributeUpdate s))
 
-toBooleanAttribute :: AttributeValue -> Maybe Bool
-toBooleanAttribute (BooleanAttribute i) = Just i
-toBooleanAttribute _ = Nothing
+data ObjectParseSpec s
+  = ObjectParseSpec String [PositionalSpec s] [AttributeSpec s]
 
-toIdentifierAttribute :: AttributeValue -> Maybe String
-toIdentifierAttribute (IdentifierAttribute i) = Just i
-toIdentifierAttribute _ = Nothing
+buildAttributeSpec :: FDFLParsable a => String -> Bool -> Validator a
+  -> Lens.Setter' s a -> AttributeSpec s
+buildAttributeSpec name required validator setter =
+  AttributeSpec name required (parseKeywordParam name validator setter)
 
-toIdentifierListAttribute :: AttributeValue -> Maybe [String]
-toIdentifierListAttribute (IdentifierListAttribute l) = Just l
-toIdentifierListAttribute _ = Nothing
+buildPositionalSpec :: FDFLParsable a => String -> Validator a
+  -> Lens.Setter' s a -> PositionalSpec s
+buildPositionalSpec name validator setter =
+  PositionalSpec name (parsePositionalParam name validator setter)
+
+alwaysValid :: Validator a
+alwaysValid = return
+
+validateList :: Validator a -> Validator [a]
+validateList = mapM
+
+knownIdentifier :: Validator Identifier
+knownIdentifier (Identifier name) = do
+  state <- getState
+  let fdfl = _psFDFL state
+  if containsSymbol fdfl name
+    then return $ Identifier name
+    else parserFail $ "Unknown identifer " ++ name
+
+parseKeywordParam :: FDFLParsable a => String -> Validator a
+  -> Lens.Setter' s a -> FDFLParser (AttributeUpdate s)
+parseKeywordParam name validator setter =  do
+  _ <- parseReserved name >> parseReservedOp "="
+  AttributeUpdate name <$> Lens.set' setter <$> (parse >>= validator)
+
+parsePositionalParam :: FDFLParsable a => String -> Validator a
+  -> Lens.Setter' s a -> FDFLParser (AttributeUpdate s)
+parsePositionalParam name validator setter =
+  AttributeUpdate name <$> Lens.set' setter <$> (parse >>= validator)
+
+parseField :: ObjectParseSpec Field
+parseField = ObjectParseSpec "Field" []
+  [ buildAttributeSpec "rank" True alwaysValid fieldRank
+  , buildAttributeSpec "name" True alwaysValid fieldName
+  , buildAttributeSpec "symmetric" False alwaysValid fieldSymmetric
+  , buildAttributeSpec "spatial_stagger_strategy" False alwaysValid fieldStaggerStrategySpatial
+  , buildAttributeSpec "temporal_stagger_strategy" False alwaysValid fieldStaggerStrategyTemporal
+  ]
+
+parseMesh :: ObjectParseSpec Mesh
+parseMesh = ObjectParseSpec "Mesh" []
+  [ buildAttributeSpec "name" True alwaysValid meshName
+  , buildAttributeSpec "dimension" True alwaysValid meshDimension
+  , buildAttributeSpec "fields" True (validateList knownIdentifier) meshFields
+  ]
+
+parseFieldUpdate :: ObjectParseSpec FieldUpdate
+parseFieldUpdate = ObjectParseSpec "FieldUpdate"
+  [ buildPositionalSpec "lhs" knownIdentifier fieldUpdateLHS
+  , buildPositionalSpec "rhs" knownIdentifier fieldUpdateRHS
+  ]
+  []
+
+validateAttributes :: [AttributeSpec s] -> [AttributeUpdate s]
+  -> Either String [AttributeUpdate s]
+validateAttributes specs updates = if not $ null duplicateAttributes
+  then Left $ "Duplicate attribute " ++ head duplicateAttributes
+  else if not $ null missing
+    then Left $ "Missing attribute " ++ head missing
+    else Right updates
+  where
+  reqAttributes = [name | AttributeSpec name req _ <- specs, req]
+  missing = [name | name <- reqAttributes, not $ Map.member name present]
+  insertWith' m (k, a) = Map.insertWith (+) k a m
+  attributeNames = [name | AttributeUpdate name _ <- updates]
+  present = foldl insertWith' Map.empty  [(name, 1::Integer) | name <- attributeNames]
+  duplicateAttributes = Map.keys $ Map.filter (> 1) present
+
+parseCommaSepSeq :: [FDFLParser a] -> FDFLParser [a]
+parseCommaSepSeq [] = return []
+parseCommaSepSeq [p] = return <$> p
+parseCommaSepSeq (p:ps) = do
+  first <- p
+  _ <- parseComma
+  rest <- parseCommaSepSeq ps
+  return (first:rest)
+
+parseSpecToParser :: FDFLObject s => ObjectParseSpec s -> FDFLParser Definition
+parseSpecToParser (ObjectParseSpec name positionalSpecs attributeSpecs) =
+  parseReserved name >> parseParens parseParams
+  where parseParams = do
+        let positionalParsers = [parser | (PositionalSpec _ parser) <- positionalSpecs]
+        let keywordParsers = [parser | (AttributeSpec _ _ parser) <- attributeSpecs]
+        positionalAttributes <- parseCommaSepSeq positionalParsers
+        maybeComma <- optionMaybe parseComma
+        let parseKeywords = case maybeComma of
+                              Just _ -> True
+                              Nothing -> null positionalAttributes
+        let unvalidatedAttributes = parseCommaSep $ choice keywordParsers
+        attributes <- if parseKeywords
+          then parserFailEither $ validateAttributes attributeSpecs <$> unvalidatedAttributes
+          else return []
+        let allAttributes = attributes ++ positionalAttributes
+        let updates = [update | (AttributeUpdate _ update) <- allAttributes]
+        return . wrapObject $ foldl (flip ($)) emptyObject updates
+
+class FDFLObject a where
+  wrapObject :: a -> Definition
+  emptyObject :: a
+
+instance FDFLObject Field where
+  wrapObject = FieldDef
+  emptyObject = Field {
+      _fieldSymmetric = False,
+      _fieldName = error "undefined fieldName",
+      _fieldRank = error "undefined fieldRank",
+      _fieldStaggerStrategySpatial = None,
+      _fieldStaggerStrategyTemporal = None
+    }
+
+instance FDFLObject Mesh where
+  wrapObject = MeshDef
+  emptyObject = Mesh {
+      _meshName = error "undefined meshName",
+      _meshDimension = error "undefined meshDimension",
+      _meshFields = []
+    }
+
+instance FDFLObject FieldUpdate where
+  wrapObject = FieldUpdateDef
+  emptyObject = FieldUpdate {
+    _fieldUpdateLHS = error "undefined fieldUpdateLHS",
+    _fieldUpdateRHS = error "undefined fieldUpdateRHS"
+  }
+
+class FDFLParsable a where
+  parse :: Parsec String FDFLParseState a
+
+instance FDFLParsable s => FDFLParsable [s] where
+  parse = parseBrackets $ parseCommaSep parse
+
+parseBoundedEnum :: (Show a, Enum a, Bounded a) => FDFLParser a
+parseBoundedEnum = choice $ toParser <$> values
+  where
+  values = [minBound .. maxBound]
+  toParser = \x -> parseReserved (show x) >> return x
+
+instance FDFLParsable Bool where
+  parse = parseBoundedEnum
+
+instance FDFLParsable StaggerStrategy where
+  parse = parseBoundedEnum
+
+instance FDFLParsable StringLiteral where
+  parse = StringLiteral <$> parseStringLiteral
+
+instance FDFLParsable Identifier where
+  parse = Identifier <$> parseIdentifier
+
+instance FDFLParsable Integer where
+  parse = parseInteger
+
+--data MeshUpdate = MeshUpdate String [FieldUpdate]
+--  deriving Show
+--
 
 containsSymbol :: FDFL -> String -> Bool
 containsSymbol fdfl sym = Map.member sym $ symbols fdfl
-
-getTypedAttribute :: (AttributeValue -> Maybe v) -> Map String AttributeValue -> String -> Either String v
-getTypedAttribute converter mapping key = case Map.lookup key mapping of
-  Just value -> case converter value of
-    Just typedValue -> Right typedValue
-    Nothing -> Left $ "Attribute " ++ show key ++ " has incorrect type"
-  Nothing ->  Left $ "Attribute " ++ show key ++ " not found"
-
-getStringAttribute :: Map String AttributeValue -> String -> Either String String
-getStringAttribute = getTypedAttribute toStringAttribute
-
-getIntegerAttribute :: Map String AttributeValue -> String -> Either String Integer
-getIntegerAttribute = getTypedAttribute toIntegerAttribute
-
-getBooleanAttribute :: Map String AttributeValue -> String -> Either String Bool
-getBooleanAttribute = getTypedAttribute toBooleanAttribute
-
-getIdentifierAttribute :: Map String AttributeValue -> String -> Either String String
-getIdentifierAttribute = getTypedAttribute toIdentifierAttribute
-
-getIdentifierListAttribute :: Map String AttributeValue -> String -> Either String [String]
-getIdentifierListAttribute = getTypedAttribute toIdentifierListAttribute
-
-getAttributeDefault :: a -> (Map String AttributeValue -> String -> Either String a) -> Map String AttributeValue -> String -> Either String a
-getAttributeDefault def getter _map _key = if Map.member _key _map
-  then getter _map _key
-  else Right def
-
-getFieldIdentifier :: FDFL -> String -> Either String String
-getFieldIdentifier fdfl ident = case Map.lookup ident $ symbols fdfl of
-  Nothing -> Left $ "Symbol " ++ show ident ++ " not defined."
-  Just sym -> case sym of
-    FieldDef _ -> Right ident
-    _ -> Left $ "Expected a field, but " ++ ident ++ " is not."
-
 
 addDefinition :: FDFL -> String -> Definition -> Either String FDFL
 addDefinition fdfl symName def = if containsSymbol fdfl symName
@@ -147,7 +260,7 @@ fdflDef :: LanguageDef st
 fdflDef = emptyDef {
   caseSensitive = True,
   reservedOpNames = ["="],
-  reservedNames = ["Field", "True", "False", "Mesh", "FieldUpdate"],
+  reservedNames = ["True", "False"],
   commentLine = "#",
   identStart = letter
 }
@@ -170,20 +283,6 @@ parseFDFL =
   eof >>
   getState
 
-listToMap :: (Show k, Ord k) => [(k, v)] -> Either String (Map k v)
-listToMap lst = foldM combine Map.empty lst
-  where
-  combine oldMap (newK, newV) = case insertLookupWithKey (\_ v _ -> v) newK newV oldMap of
-    (Nothing, newMap) -> Right newMap
-    (Just _, _) -> Left $ "Duplicate attribute " ++ show newK
-
-listToSet :: (Show k, Ord k) => [k] -> Either String (Set  k)
-listToSet lst = foldM combine Set.empty lst
-  where
-  combine set newK = if Set.member newK set
-    then Left $ "Duplicate identifier " ++ show newK
-    else Right $ Set.insert newK set
-
 parserFailEither :: ParsecT s u m (Either String a) -> ParsecT s u m a
 parserFailEither p = do
   parsed <- p
@@ -196,91 +295,19 @@ parseAssignment = do
   symbolName <- parseIdentifier
   _ <- parseReservedOp "="
   oldState <- getState
-  newState <- (flip (Lens.set psFDFL) oldState) <$> (parserFailEither $ addDefinition (_psFDFL oldState) symbolName <$> parseDefinition)
+  let addSymbol = addDefinition (_psFDFL oldState) symbolName
+  let setFDFL = flip (Lens.set psFDFL) oldState
+  newState <- setFDFL <$> (parserFailEither $ addSymbol <$> parseDefinition)
   putState newState
   return ()
 
 parseDefinition :: FDFLParser Definition
-parseDefinition = FieldDef <$> parseFieldDefinition <|>
-                  MeshDef <$> parseMeshDefinition <|>
-                  FieldUpdateDef <$> parseFieldUpdateDefinition
-
-parseFieldDefinition :: FDFLParser Field
-parseFieldDefinition = do
-  parseReserved "Field"
-  attributeMap <- parserFailEither . parsecMap listToMap . parseParens $ parseCommaSep parseAttribute
-  case attributesToField attributeMap of
-    Left str -> parserFail str
-    Right field -> return field
-
-parseMeshDefinition :: FDFLParser Mesh
-parseMeshDefinition = do
-  parseReserved "Mesh"
-  attributeMap <- parserFailEither . parsecMap listToMap . parseParens $ parseCommaSep parseAttribute
-  fdfl <- _psFDFL <$> getState
-  case attributesToMesh fdfl attributeMap of
-    Left str -> parserFail str
-    Right mesh -> return mesh
-
-parseFieldUpdateDefinition :: FDFLParser FieldUpdate
-parseFieldUpdateDefinition = do
-  parseReserved "FieldUpdate"
-  (name, value) <- parseParens parseArguments
-  return $ FieldUpdate name value
-  where
-    parseArguments = do
-      fdfl <- _psFDFL <$> getState
-      field <- parserFailEither $ getFieldIdentifier fdfl <$> parseIdentifier
-      _ <- parseComma
-      value <- parseFieldExpr
-      return (field, value)
-
-parseFieldExpr :: FDFLParser FieldExpr
-parseFieldExpr = do
-  fdfl <- _psFDFL <$> getState
-  FieldRef <$> (parserFailEither $ getFieldIdentifier fdfl <$> parseIdentifier)
-
-attributesToField :: Map String AttributeValue -> Either String Field
-attributesToField _map = do
-  name <- getStringAttribute _map "name"
-  rank <- getIntegerAttribute _map "rank"
-  sym <- (getAttributeDefault False getBooleanAttribute) _map "symmetric"
-  return Field {
-    fieldName = name,
-    fieldRank = rank,
-    fieldSymmetric = sym,
-    fieldTemporalStaggering = [],
-    fieldSpatialStaggering = []
-  }
-
-attributesToMesh :: FDFL -> Map String AttributeValue -> Either String Mesh
-attributesToMesh fdfl _map = do
-  name <- getStringAttribute _map "name"
-  dim <- getIntegerAttribute _map "dimension"
-  fields <- (getIdentifierListAttribute _map "fields") >>= mapM (getFieldIdentifier fdfl) >>= listToSet
-  return Mesh {meshName = name, meshDimension = dim, meshFields = Set.toList fields}
-
-parseAttribute :: FDFLParser (String, AttributeValue)
-parseAttribute = do
-  name <- parseIdentifier
-  _ <- parseReservedOp "="
-  value <- parseAttributeValue
-  return (name, value)
-
-parseAttributeValue :: FDFLParser AttributeValue
-parseAttributeValue = choice
-  [ IdentifierAttribute <$> parseIdentifier
-  , StringAttribute <$> parseStringLiteral
-  , IntegerAttribute <$> parseInteger
-  , BooleanAttribute <$> parseBool
-  , IdentifierListAttribute <$> (parseBrackets $ parseCommaSep parseIdentifier)
-  ]
-
-parseBool :: FDFLParser Bool
-parseBool = choice
-  [ parseReserved "True" >> return True
-  , parseReserved "False" >> return False
+parseDefinition = choice
+  [ parseSpecToParser parseField
+  , parseSpecToParser parseMesh
+  , parseSpecToParser parseFieldUpdate
   ]
 
 parseInput :: String -> String -> Either ParseError FDFL
-parseInput sourceName s = _psFDFL <$> runParser parseFDFL emptyFDFLParseState sourceName s
+parseInput sourceName s
+  = _psFDFL <$> runParser parseFDFL emptyFDFLParseState sourceName s
