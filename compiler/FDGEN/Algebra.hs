e6a8a52d64456c2d872bda3532b641629a3f41b6
Francis Russell <francis@unchartedbackwaters.co.uk>
2016-08-23 18:50:30 +0100

    Work on expression simplification.

--- compiler/FDGEN/Algebra.hs
+++ compiler/FDGEN/Algebra.hs
@@ -5,6 +5,10 @@ import Data.Ratio ((%), denominator, numerator)
 import qualified Data.Map as Map
 import qualified Control.Lens as Lens
 
+data SumTag
+
+data ProdTag
+
 data Expression e
   = Symbol e
   | Sum (PairSeq SumTag e)
@@ -22,23 +26,21 @@ data RewriteState
   deriving (Eq, Ord, Show)
 
 data PairSeq t e = PairSeq {
-  _overall :: Rational,
-  _terms :: Map (Expression e) Rational,
-  _rewriteState :: RewriteState
+  _psOverall :: Rational,
+  _psTerms :: Map (Expression e) Rational,
+  _psRewriteState :: RewriteState
 } deriving (Eq, Ord, Show)
 
-data SumTag
-
-data ProdTag
+Lens.makeLenses ''PairSeq
 
 class PairSeqLike t e where
   empty :: PairSeq t e
   extractMultipliers :: Ord e => PairSeq t e -> PairSeq t e
   wrap :: PairSeq t e -> Expression e
-  addTerm :: Ord e => PairSeq t e -> Expression e -> Rational -> PairSeq t e
+  addTerm :: Ord e => Expression e -> Rational -> PairSeq t e -> PairSeq t e
 
 isNormalised :: PairSeq t e -> Bool
-isNormalised seq' = case _rewriteState seq' of
+isNormalised seq' = case _psRewriteState seq' of
   Normal -> True
   NormalAndExtracted -> True
   _ -> False
@@ -46,128 +48,142 @@ isNormalised seq' = case _rewriteState seq' of
 normalise :: (PairSeqLike t e, Ord e) => PairSeq t e -> PairSeq t e
 normalise seq' = if isNormalised seq'
   then seq'
-  else removeZeros $ extractMultipliers seq'
+  else Lens.set psRewriteState Normal . removeZeros $ extractMultipliers seq'
 
-extractMultiplier :: Ord e => Expression e -> (Rational, Expression e)
-extractMultiplier (Product seq') = case _rewriteState seq' of
-  NormalAndExtracted -> (1, Product seq')
-  _ -> (overall, Product updated)
+extractMultiplier :: forall e . Ord e => Expression e -> (Rational, Expression e)
+extractMultiplier (Product seq') = case _psRewriteState seq' of
+  NormalAndExtracted -> (1, simplifyPairSeq seq')
+  _ -> (overall, simplifyPairSeq updated)
        where
        normalised = normalise seq'
-       overall = _overall normalised
-       updated = normalised { _overall = 1, _rewriteState = NormalAndExtracted }
-extractMultiplier e = (1, e)
+       overall = _psOverall normalised
+       updated :: PairSeq ProdTag e
+       updated = normalised { _psOverall = 1, _psRewriteState = NormalAndExtracted }
+extractMultiplier (Sum seq') = case _psRewriteState seq' of
+  NormalAndExtracted -> (1, simplifyPairSeq seq')
+  _ -> (common, simplifyPairSeq updated)
+       where
+       normalised = normalise seq'
+       overall = _psOverall normalised
+       terms = _psTerms normalised
+       gcd' r1 r2 = (gcd (numerator r1) (numerator r2)) % (gcd (denominator r1) (denominator r2))
+       common = foldl gcd' 1 (overall:(Map.elems terms))
+       overall' = (_psOverall normalised) / common
+       terms' = Map.map (/ common) $ _psTerms normalised
+       updated :: PairSeq SumTag e
+       updated = PairSeq { _psOverall = overall', _psRewriteState = NormalAndExtracted, _psTerms = terms' }
+extractMultiplier e = (1, simplify e)
 
 instance PairSeqLike SumTag e where
   empty = PairSeq {
-    _overall = 0,
-    _terms = Map.empty,
-    _rewriteState = NonNormal
+    _psOverall = 0,
+    _psTerms = Map.empty,
+    _psRewriteState = NonNormal
   }
-  extractMultipliers seq' = Map.foldlWithKey addScaledTerm base (_terms seq')
+  extractMultipliers seq' = Map.foldlWithKey addScaledTerm base (_psTerms seq')
     where
-    base = (empty :: PairSeq SumTag e) { _overall = _overall seq' }
+    base = (empty :: PairSeq SumTag e) { _psOverall = _psOverall seq' }
     addScaledTerm :: PairSeq SumTag e -> Expression e -> Rational -> PairSeq SumTag e
-    addScaledTerm ps expr coeff = addTerm ps expr' $ coeff * multiplier
+    addScaledTerm ps expr coeff = addTerm expr' (coeff * multiplier) ps
       where
       (multiplier, expr') = extractMultiplier expr
   wrap = Sum
-  addTerm seq' expr coeff = newSeq
+  addTerm expr coeff seq' = newSeq
     where
     newSeq = case expr of
-      ConstantRational r -> seq' { _overall = (_overall seq') + (r * coeff) }
-      Sum subSeq -> Map.foldlWithKey addTerm' incorporateOverall (_terms subSeq)
+      ConstantRational r -> Lens.over psOverall (+ (r * coeff)) seq'
+      Sum subSeq -> Map.foldlWithKey addTerm' incorporateOverall (_psTerms subSeq)
        where
-       incorporateOverall = addTerm seq' (ConstantRational $ _overall subSeq) coeff
-       addTerm' s e c = addTerm s e $ c * coeff
+       incorporateOverall = addTerm (ConstantRational $ _psOverall subSeq) coeff seq'
+       addTerm' s e c = addTerm e (c * coeff) s
       _ -> normalInsert
       where
-      normalInsert = seq' { _terms = Map.insertWith (+) expr coeff (_terms seq') }
+      normalInsert = incrementTerm expr coeff seq'
 
 instance PairSeqLike ProdTag e where
   empty = PairSeq {
-    _overall = 1,
-    _terms = Map.empty,
-    _rewriteState = NonNormal
+    _psOverall = 1,
+    _psTerms = Map.empty,
+    _psRewriteState = NonNormal
   }
-  extractMultipliers seq' = Map.foldlWithKey addScaledTerm base (_terms seq')
+  extractMultipliers seq' = Map.foldlWithKey addScaledTerm base (_psTerms seq')
     where
-    base = (empty :: PairSeq ProdTag e) { _overall = _overall seq' }
+    base = (empty :: PairSeq ProdTag e) { _psOverall = _psOverall seq' }
     addScaledTerm :: PairSeq ProdTag e -> Expression e -> Rational -> PairSeq ProdTag e
     addScaledTerm ps expr coeff = newSeq
       where
       (multiplier, expr') = extractMultiplier expr
       isIntegerCoeff = denominator coeff == 1
       newSeq = if isIntegerCoeff
-        then addTerm (addTerm ps (ConstantRational multiplier) coeff) expr' coeff
-        else addTerm ps expr coeff
+        then foldl (flip $ uncurry addTerm) ps [(ConstantRational multiplier, coeff), (expr', coeff)]
+        else addTerm expr coeff ps
   wrap = Product
-  addTerm seq' expr coeff = newSeq
+  addTerm expr coeff seq' = newSeq
     where
     isIntegerCoeff = denominator coeff == 1
     newSeq = if isIntegerCoeff
       then case expr of
-        ConstantRational r -> seq' { _overall = (_overall seq') * (r ^^ numerator coeff) }
-        Product subSeq -> Map.foldlWithKey addTerm' incorporateOverall (_terms subSeq)
+        ConstantRational r -> Lens.over psOverall (* (r ^^ numerator coeff)) seq'
+        Product subSeq -> Map.foldlWithKey addTerm' incorporateOverall (_psTerms subSeq)
          where
-         incorporateOverall = addTerm seq' (ConstantRational $ _overall subSeq) coeff
-         addTerm' s e c = addTerm s e $ c * coeff
+         incorporateOverall = addTerm (ConstantRational $ _psOverall subSeq) coeff seq'
+         addTerm' s e c = addTerm e (c * coeff) s
         _ -> normalInsert
       else normalInsert
       where
-      normalInsert = seq' { _terms = Map.insertWith (+) expr coeff (_terms seq') }
+      normalInsert = incrementTerm expr coeff seq'
+
+incrementTerm :: Ord e => Expression e -> Rational -> PairSeq t e -> PairSeq t e
+incrementTerm expr coeff = Lens.over psTerms (Map.insertWith (+) expr coeff)
 
 removeZeros :: PairSeq t e -> PairSeq t e
-removeZeros seq' = seq' { _terms = terms' }
+removeZeros seq' = seq' { _psTerms = terms' }
   where
-  terms' = Map.filter (/= 0) $ _terms seq'
-
-Lens.makeLenses ''PairSeq
+  terms' = Map.filter (/= 0) $ _psTerms seq'
 
 add :: Ord e => Expression e -> Expression e -> Expression e
-add a b = Sum $ PairSeq {
-  _overall = 0,
-  _terms = Map.fromList [(a, 1), (b, 1)],
-  _rewriteState = NonNormal
-}
+add a b = Sum $ foldr (uncurry addTerm) empty [(a, 1), (b, 1)]
 
 sub :: Ord e => Expression e -> Expression e -> Expression e
-sub a b = Sum $ PairSeq {
-  _overall = 0,
-  _terms = Map.fromList [(a, 1), (b, -1)],
-  _rewriteState = NonNormal
-}
+sub a b = Sum $ foldr (uncurry addTerm) empty [(a, 1), (b, -1)]
 
 mul :: Ord e => Expression e -> Expression e -> Expression e
-mul a b = Product $ PairSeq {
-  _overall = 1,
-  _terms = Map.fromList [(a, 1), (b, 1)],
-  _rewriteState = NonNormal
-}
+mul a b = Product $ foldr (uncurry addTerm) empty [(a, 1), (b, 1)]
+
+divide :: Ord e => Expression e -> Expression e -> Expression e
+divide a b = Sum $ foldr (uncurry addTerm) empty [(a, 1), (b, -1)]
 
 hasNullOverall :: forall t e . PairSeqLike t e => PairSeq t e -> Bool
-hasNullOverall seq' = (_overall seq') == (_overall (empty :: PairSeq t e))
+hasNullOverall seq' = (_psOverall seq') == (_psOverall (empty :: PairSeq t e))
 
 simplify :: Ord e => Expression e -> Expression e
-simplify (Product seq') = simplifyPairSeq seq';
-simplify (Sum seq') = simplifyPairSeq seq';
+simplify (Product seq') = simplifyPairSeq seq'
+simplify (Sum seq') = simplifyPairSeq seq'
+simplify (Abs (ConstantRational r)) = ConstantRational $ abs r
+simplify (Signum (ConstantRational r)) = ConstantRational $ signum r
+simplify (Abs (ConstantFloat f)) = ConstantFloat $ abs f
+simplify (Signum (ConstantFloat f)) = ConstantFloat $ signum f
 simplify a = a
 
 simplifyPairSeq :: (PairSeqLike t e, Ord e) => PairSeq t e -> Expression e
 simplifyPairSeq seq' = newExpr
   where
   normalised = normalise seq'
-  newExpr = case Map.toList (_terms normalised) of
-    [] -> ConstantRational $ _overall normalised
+  newExpr = case Map.toList (_psTerms normalised) of
+    [] -> ConstantRational $ _psOverall normalised
     [(a, 1)] -> if (hasNullOverall normalised)
       then a
       else wrap normalised
     _ -> wrap normalised
 
 instance Ord e => Num (Expression e) where
-  fromInteger a = ConstantRational (a % 1)
-  (+) a b = simplify $ add a b
-  (-) a b = simplify $ sub a b
-  (*) a b = simplify $ mul a b
+  fromInteger = ConstantRational . fromInteger
+  (+) a = simplify . add a
+  (-) a = simplify . sub a
+  (*) a = simplify . mul a
   abs = simplify . Abs
   signum = simplify . Signum
+
+instance Ord e => Fractional (Expression e) where
+  fromRational = ConstantRational
+  (/) a = simplify . divide a
