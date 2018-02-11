dee3c7ba0de7376e3a5c8b27347d8335a78b14c3
Francis Russell <francis@unchartedbackwaters.co.uk>
2018-02-11 01:16:38 +0000

    Rework stencil offsets on staggered grids.

--- compiler/FDGEN/Stencil.hs
+++ compiler/FDGEN/Stencil.hs
@@ -4,6 +4,7 @@ import Control.Exception (assert)
 import Data.Char (chr, ord)
 import Data.List (genericIndex, genericLength)
 import Data.Map (Map)
+import Data.Ratio ((%))
 import FDGEN.Algebra (Expression(..), expand, lagrange, subst, substSymbols, diff, vars, polyCoeff)
 import FDGEN.Pretty (PrettyPrintable, toDoc)
 import Text.PrettyPrint as PrettyPrint
@@ -44,6 +45,15 @@ instance PrettyPrintable StencilTerminal
     where
     indexToString i = [chr $ ord 'x' + fromIntegral i]
 
+staggerToInteger :: Stagger -> Integer
+staggerToInteger stagger = case stagger of
+  StaggerNone -> 0
+  StaggerPos -> 1
+  StaggerNeg -> -1
+
+calculateStencilOffset :: Integer -> Stagger -> Integer
+calculateStencilOffset width stagger = - (width + staggerToInteger stagger) `div` 2
+
 buildStencil :: StencilSpec -> Stencil
 buildStencil spec = assert (length derivatives == length staggering) result
   where
@@ -58,7 +68,7 @@ buildStencil spec = assert (length derivatives == length staggering) result
     then 1
     else order + derivative
   stencilWidths = uncurry stencilWidth <$> zip staggering derivatives
-  originGrid = (`div` 2) <$> stencilWidths
+  originGrid = (negate . uncurry calculateStencilOffset) <$> zip stencilWidths staggering
   originSubstitutions = (\dim -> (Symbol $ Position dim, 0)) <$> [0..numDimensions - 1]
   interpolation = buildInterpolation $ zip3 stencilWidths originGrid staggering
   diff' sym power expression = genericIndex (iterate (diff sym) expression) power
@@ -76,10 +86,7 @@ buildStencil spec = assert (length derivatives == length staggering) result
       extrude _ terminal = terminal
       extrudeExpression position = substSymbols (extrude position)
       extruded = [((fromInteger i + staggerOffset), extrudeExpression i lowerInterpolation) | i <- (subtract centrePoint) <$> [0..width - 1]]
-      staggerOffset = case stagger of
-        StaggerNone -> 0.0
-        StaggerPos -> 0.5
-        StaggerNeg -> -0.5
+      staggerOffset = fromRational $ staggerToInteger stagger % 2
 
 expressionToStencil :: Integer -> Expression StencilTerminal -> Stencil
 expressionToStencil dim expression = Stencil
